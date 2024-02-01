46ab06a1d200fea459098e68dcd25d16345d3137
FelicityS1 <125722817+FelicityS1@users.noreply.github.com>
2024-02-02 05:10:57 +0800

    SalesCalculator.java

--- JavaApplication1/src/SalesCalculator.java
+++ JavaApplication1/src/SalesCalculator.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-// Abstract class representing the basic structure of an item
+// Abstract class 
 abstract class Item {
     double price;
 
